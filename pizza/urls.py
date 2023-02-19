1c628ed3c426eee663e219d6020d192bce44c9cd
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-19 18:29:28 +0300

    changes on views and urls

--- pizza/urls.py
+++ pizza/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls'))
+    path('auth/', include('authentication.urls')),
+    path('orders/', include('orders.url')),
     
 ]
