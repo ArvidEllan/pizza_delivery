35cccf076a6f47a9c9c9be9d5a5542fa75fdcd8e
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-20 11:55:49 +0300

    functionality for super user

--- authentication/models.py
+++ authentication/models.py
@@ -20,4 +20,13 @@ class CustomUserManager(BaseUserManager):
         return new_user
     
     def create_superuser(self,email,password,**extra_fields):
-        pass 
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_("Superuser should have is_staff as True"))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_("Superuser should have is_superuser as True"))
+        if extra_fields.get('is_active') is not True:
+            raise ValueError(_("Superuser should have is_active as True"))
