e579aac359e6f13c87cb1e401bb38874c2a46d59
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-20 11:49:45 +0300

    creating normal users functions

--- authentication/models.py
+++ authentication/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
     
     def create_user(self,email,password,**extra_fields):
-        pass
+        if not email:
+            raise ValueError(_("email should be provided"))
+        email=self.normalize_email(email)
+        
+        new_user=self.model(email,**extra_fields)
+        
+        new_user.set_password(password)
+        
+        new_user.save()
+        
+        return new_user
     
     def create_superuser(self,email,password,**extra_fields):
         pass 
