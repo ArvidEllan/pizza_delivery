d72e253566ce50d2158e8b8e88392723b4748796
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-22 15:29:00 +0300

    changes in validation serializers

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -6,3 +6,30 @@ class UserCreationSerializer(serializers.ModelSerializer):
     username=serializers.CharField(max_length=25)
     email=serializers.EmailField(max_length=80)
     phone_number=PhoneNumberField(allow_null=False,allow_blank=False) 
+    password=serializers.CharField(min_length=8)
+    
+    class Meta:
+        model=User
+        fields=['username','email','phone_number','password']
+        
+        
+    def validate(self, attrs):
+        username_exist=User.objects.filter(username=attrs['username']).exists()
+        
+        if username_exist:
+            raise serializers.ValidationError("The username exists")
+        
+        email_exist=User.objects.filter(username=attrs['email']).exists()
+        
+        if email_exist:
+            raise serializers.ValidationError("A User with email exists")
+        
+        
+        phonenumber_exist=User.objects.filter(username=attrs['phone_number']).exists()
+        
+        if username_exist:
+            raise serializers.ValidationError("A user  with that phonenumber exists")
+         
+        
+        
+        return super().validate(attrs)
