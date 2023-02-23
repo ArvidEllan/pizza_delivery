e5d9daeabf95a6dfb2986bd4bc210f9b441a8ecc
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-23 15:04:21 +0300

    access and refresh token on settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,6 +66,8 @@ REST_FRAMEWORK={
 SIMPLE_JWT = {
     
     'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME' : timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5)
     
 }
 
